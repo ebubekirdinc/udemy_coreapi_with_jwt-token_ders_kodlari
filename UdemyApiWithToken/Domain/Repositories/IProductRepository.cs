3608e802e0b619cb98e0d8c2c8672adaed621beb
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-05-09 17:48:32 +0300

    Ders 8-10(ConnectionString bağlantımızın startup.cs  sınıfına taşınması-IProductRepository interface'sinin oluşturulması)

---
+++ UdemyApiWithToken/Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace UdemyApiWithToken.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> ListAsync();
+
+        Task AddProductAsync(Product product);
+
+        Task RemoveProductAsync(int productId);
+
+        Task UpdateProductAsync(Product product);
+
+        Task<Product> FindByIdAsync(int productId);
+    }
+}
