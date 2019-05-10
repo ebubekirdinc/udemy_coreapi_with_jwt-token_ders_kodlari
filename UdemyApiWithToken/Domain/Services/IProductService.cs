a4fa3064e7e91335741aeb1dcc8afebd25d16b90
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-05-11 02:02:09 +0300

    Ders 15(IProductService interface'sinin oluşturulması)

---
+++ UdemyApiWithToken/Domain/Services/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using UdemyApiWithToken.Domain.Responses;
+
+namespace UdemyApiWithToken.Domain.Services
+{
+    internal interface IProductService
+    {
+        Task<ProductListResponse> ListAsync();
+
+        Task<ProductResponse> AddProduct(Product product);
+
+        Task<ProductResponse> RemoveProduct(int productId);
+
+        Task<ProductResponse> UpdateResponse(Product product, int productId);
+
+        Task<Product> FindByIdAsync(int productId);
+    }
+}
