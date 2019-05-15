442a6e4a9054456100f5f085a320b00bf69a92b9
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-05-15 11:20:47 +0300

    Ders 36(ITokenHandler interface'sinin oluşturulması)

---
+++ UdemyApiWithToken/Security/Token/ITokenHandler.cs
@@ -0,0 +1,11 @@
+﻿using UdemyApiWithToken.Domain;
+
+namespace UdemyApiWithToken.Security.Token
+{
+    internal interface ITokenHandler
+    {
+        AccessToken CreateAccessToken(User user);
+
+        void RevokeRefreshToken(User user);
+    }
+}
