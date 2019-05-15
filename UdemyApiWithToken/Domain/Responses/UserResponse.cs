b95e340633f40b8583af78bd6ad2b7ce74031a86
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-05-15 15:41:26 +0300

    Ders 43(UserResponse class'sının oluşturulması)

---
+++ UdemyApiWithToken/Domain/Responses/UserResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UdemyApiWithToken.Domain.Responses
+{
+    public class UserResponse : BaseResponse
+    {
+        public User user { get; set; }
+
+        private UserResponse(Boolean success, string message, User user) : base(success, message)
+        {
+            this.user = user;
+        }
+
+        //success
+
+        public UserResponse(User user) : this(true, String.Empty, user)
+        {
+        }
+
+        //fail
+
+        public UserResponse(string message) : this(false, message, null)
+        {
+        }
+    }
+}
