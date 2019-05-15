50391dbc739e629c05025678ab647d3a840058b1
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-05-15 14:50:24 +0300

    Ders 41(IUserRepository interface'sinin oluşturulması)

---
+++ UdemyApiWithToken/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace UdemyApiWithToken.Domain.Repositories
+{
+    internal interface IUserRepository
+    {
+        Task AddUser(User user);
+
+        User FindById(int userId);
+
+        User FindByEmailandPassword(string email, string password);
+
+        void SaveRefreshToken(int userId, string refreshToken);
+
+        User GetUserWithRefreshToken(string refreshToken);
+
+        void RemoveRefreshToken(User user);
+    }
+}
