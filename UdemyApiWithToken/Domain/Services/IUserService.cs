f1df8d9105c960a6b3065f02b164c63d4d8843d0
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-05-15 15:58:15 +0300

    Ders 44(IUserService class'sının oluşturulması)

---
+++ UdemyApiWithToken/Domain/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using UdemyApiWithToken.Domain.Responses;
+
+namespace UdemyApiWithToken.Domain.Services
+{
+    internal interface IUserService
+    {
+        UserResponse AddUser(User user);
+
+        UserResponse FindById(int userId);
+
+        UserResponse FindEmailAndPassword(string email, string password);
+
+        void SaveRefreshToken(int userId, string refreshToken);
+
+        UserResponse GetUserWithRefreshToken(string refreshToken);
+
+        void RemoveRefreshToken(User user);
+    }
+}
