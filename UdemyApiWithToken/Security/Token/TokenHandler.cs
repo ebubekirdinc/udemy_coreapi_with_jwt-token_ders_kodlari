4607521d366fd5cafa3dc851ac8417b7c02b069a
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-05-15 12:30:55 +0300

    Ders 38(TokenHandler "CreateAccessToken" methodunun oluşturulması)

--- UdemyApiWithToken/Security/Token/TokenHandler.cs
+++ UdemyApiWithToken/Security/Token/TokenHandler.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using UdemyApiWithToken.Domain;
 
 namespace UdemyApiWithToken.Security.Token
@@ -15,7 +19,46 @@ namespace UdemyApiWithToken.Security.Token
 
         public AccessToken CreateAccessToken(User user)
         {
-            throw new NotImplementedException();
+            var accessTokenExpiration = DateTime.Now.AddMinutes(tokenOptions.AccessTokenExpiration);
+
+            var securityKey = SignHandler.GetSecurityKey(tokenOptions.SecurityKey);
+
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(
+                issuer: tokenOptions.Issuer,
+                audience: tokenOptions.Audience,
+                expires: accessTokenExpiration,
+                notBefore: DateTime.Now,
+                claims: GetClaim(user),
+                signingCredentials: signingCredentials
+
+                );
+
+            var handler = new JwtSecurityTokenHandler();
+
+            var token = handler.WriteToken(jwtSecurityToken);
+
+            AccessToken accessToken = new AccessToken();
+
+            accessToken.Token = token;
+            accessToken.RefreshToken = null;
+            accessToken.Expiration = accessTokenExpiration;
+
+            return accessToken;
+        }
+
+        private IEnumerable<Claim> GetClaim(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email,user.Email),
+                new Claim(ClaimTypes.Name,$"{user.Name} {user.SurName}"),
+                new  Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            return claims;
         }
 
         public void RevokeRefreshToken(User user)
