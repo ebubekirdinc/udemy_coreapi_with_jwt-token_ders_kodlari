29d27f2b7e5515701be92e0c3146a307d5b9c8a4
ebubekir dinç <ebubekirdinc@gmail.com>
2019-07-03 12:56:52 +0300

    ctor aciklama

--- UdemyApiWithToken/Domain/Responses/UserResponse.cs
+++ UdemyApiWithToken/Domain/Responses/UserResponse.cs
@@ -6,20 +6,20 @@ namespace UdemyApiWithToken.Domain.Responses
     {
         public User user { get; set; }
 
-        private UserResponse(Boolean success, string message, User user) : base(success, message)
+        private UserResponse(Boolean success, string message, User user) : base(success, message)//base ile miras aldigi classin ctoruna gidip set ediyor
         {
             this.user = user;
         }
 
         //success
 
-        public UserResponse(User user) : this(true, String.Empty, user)
+        public UserResponse(User user) : this(true, String.Empty, user) //this ile yukaridaki ctora gidip set ediyor
         {
         }
 
         //fail
 
-        public UserResponse(string message) : this(false, message, null)
+        public UserResponse(string message) : this(false, message, null) //this ile yukaridaki ctora gidip set ediyor
         {
         }
     }
